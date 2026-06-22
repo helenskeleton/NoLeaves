41bb4e08379548c3a77bbc10e6ad04981dc1f3e0
helenskeleton <blackusmokemefr@gmail.com>
2026-06-22 23:38:11 +0100

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -2,7 +2,7 @@ namespace NoLeaves
 {
     public static class PluginInfo
     {
-        public const string PLUGIN_GUID = "com.femboyalyssa.noleaves";
+        public const string PLUGIN_GUID = "com.poopoovr.noleaves";
         public const string PLUGIN_NAME = "NoLeaves";
         public const string PLUGIN_VERSION = "1.1.4";
     }
