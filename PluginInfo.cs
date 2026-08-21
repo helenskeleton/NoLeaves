cc5404ab8182c20b339aa60f99bed3d26d61ace8
helenskeleton <blackusmokemefr@gmail.com>
2026-08-21 03:10:45 +0100

    Change PLUGIN_GUID to new identifier

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -2,7 +2,7 @@ namespace NoLeaves
 {
     public static class PluginInfo
     {
-        public const string PLUGIN_GUID = "com.femboyalyssa.noleaves";
+        public const string PLUGIN_GUID = "0000.poopoovr.noleaves";
         public const string PLUGIN_NAME = "NoLeaves";
         public const string PLUGIN_VERSION = "1.1.4";
     }
